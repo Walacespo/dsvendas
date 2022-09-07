3942df93cd32cc8c5674e6bf96a9dbe9a3d8008d
Walace da Silva <walacespo@gmail.com>
2022-09-07 11:38:11 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Walacespo/" target="_blank" rel="noreferrer">Walace da Silva</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
